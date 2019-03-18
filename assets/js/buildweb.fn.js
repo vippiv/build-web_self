dd79b25eefbe2a69b8ae44a30be50aeb09fdba10
zwl <470211273@qq.com>
2019-03-18 17:41:33 +0800

    自助建页面

--- assets/js/buildweb.fn.js
+++ assets/js/buildweb.fn.js
@@ -12,7 +12,7 @@ function addElementToDoc() {
     // 获取要添加到文档元素的类型 TODO 这里需要根据参数判断调用哪种元素
     var htmlStr = config.baseBlockHtmlStr
 
-    // 向文档添加元素
+    // 向文档添加元素 TODO 这里要获取当前活动元素，这个元素和重要
     config.currentEle = config.buildWebBox.append(htmlStr)
 
     console.log(config.currentEle[0])
